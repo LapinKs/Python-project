0106fb4072f14afd17b851cec9151e1d6292fb74
kosty <kostyalapin777@mail.ru>
2024-01-21 17:30:16 +0500

    add fixess

---
+++ webUlearn/ulearn_lapin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UlearnLapinConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ulearn_lapin'
---
+++ webUlearn/ulearn_lapin/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+def main_page(request):
+    return render(request,'Main_page.html')
+
+def geography_page(request):
+    return render(request,'Geography.html')
+
+def last_vacancies_page(request):
+    return render(request,'Last_vacancies.html')
+
+def revelance_page(request):
+    return render(request,'Revelance.html')
+
+def skillset_page(request):
+    return render(request,'Skillset.html')
